84cca37db9a58088a2ee875d5d1c5e43b71d9adf
elpolloloco42 <pierrick.viart@epitech.eu>
2017-03-27 09:06:34 +0200

    [REBOOT] Implement Multi Feature

---
+++ Source/Paper2DTestReseau/MyClass.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class PAPER2DTESTRESEAU_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
