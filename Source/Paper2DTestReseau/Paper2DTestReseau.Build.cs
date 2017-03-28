03a92909d9adb02d357c5d70ecdb08b7b356512c
elpolloloco42 <pierrick.viart@epitech.eu>
2017-03-28 17:01:08 +0200

    [IMPLEMENT] StickMan character

--- Source/Paper2DTestReseau/Paper2DTestReseau.Build.cs
+++ Source/Paper2DTestReseau/Paper2DTestReseau.Build.cs
@@ -18,7 +18,7 @@ public class Paper2DTestReseau : ModuleRules
 		// Uncomment if you are using online features
 		PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
-		//DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
+		DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
 
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
